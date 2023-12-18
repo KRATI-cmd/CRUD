c36f49346f0c1a84aafc047c19a10da34e785575
KRATI-cmd <76157643+KRATI-cmd@users.noreply.github.com>
2023-12-18 10:27:25 +0530

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC-gvNV9fCvlcqv3y_jlmVEzQEsDFbKdoM",
+  authDomain: "myfireprowithcrud-43810.firebaseapp.com",
+  databaseURL: "https://myfireprowithcrud-43810-default-rtdb.firebaseio.com",
+  projectId: "myfireprowithcrud-43810",
+  storageBucket: "myfireprowithcrud-43810.appspot.com",
+  messagingSenderId: "106117934475",
+  appId: "1:106117934475:web:29730b7d95ad537796d38e",
+  measurementId: "G-N08QJH2VZF"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
